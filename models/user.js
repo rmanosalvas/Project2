3ae69911444f4cc10602e4af58cea348c4ca3100
Don Tran <mr.dontran@gmail.com>
2020-08-18 18:53:27 -0400

    added models

---
+++ models/user.js
@@ -0,0 +1,78 @@
+module.exports = function(sequelize, DataTypes) {
+    let User = sequelize.define("User", {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notNull: {
+                    msg: "Please enter your first name"
+                }
+            }
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                notNull: {
+                    msg: "Please enter your last name"
+                }
+            }
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate:{
+                notNull: {
+                    msg: "Please enter a username"
+                }
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate:{
+                isInt: true,
+                isLowerCase: true,
+                isUpperCase: true,
+                len: [6],
+                notNull: {
+                    msg: "Please enter a password"
+                }
+            }
+
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            isEmail: true,
+            validate:{
+                notNull: {
+                    msg: "Please enter your email"
+                }
+            }
+        },
+        userPref1: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: {
+                
+            }
+        },
+        userPref2: {
+            type: DataTypes.STRING,
+
+        },
+        userPref3: {
+            type: DataTypes.STRING,
+
+        }
+    })
+User.associate = function(models){
+    Author.hasMany(models.Post, {
+        onDelete: "cascade"
+    })
+}
+return User;
+
+}
--- schema.sql
+++ schema.sql
@@ -2,3 +2,14 @@
 DROP DATABASE IF EXISTS dateapp;
 -- Creates the "blogger" database --
 CREATE DATABASE dateapp;
+
+
+CREATE TABLE users
+(
+	id int NOT NULL AUTO_INCREMENT,
+	username varchar(255) NOT NULL,
+	password  default false,
+	email VARCHAR(255) not NULL,
+    
+    PRIMARY KEY (id)
+);
