28a39b05f260638f667ad9a17d0cf50f353f8b76
Ricardo <manosalvas2013@gmail.com>
2020-08-18 19:37:36 -0400

    pulling don info

---
+++ public/js/login.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".log-in").on("click", function (event) {
+        event.preventDefault();
+
+    });
+    $(".register-user").on("click", function (event) {
+        event.preventDefault();
+
+    });
+})
