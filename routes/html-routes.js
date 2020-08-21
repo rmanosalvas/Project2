b34a0ff85990671b4a1a79f6a1a6e8c88a9c179f
Ricardo <manosalvas2013@gmail.com>
2020-08-21 14:19:08 -0400

    adding james changes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -15,20 +15,20 @@ router.get("/signup", function(req, res) {
     res.render("signup");
 });
   
-router.get("/dashboard", function(req, res) {
-    post.all(function (allPosts){
-        let hbsObj = {
-            postTitle: allPosts.title,
-            postCategory: allPosts.category
-            allPosts.location
-            allPosts.body
+// router.get("/dashboard", function(req, res) {
+//     post.all(function (allPosts){
+//         let hbsObj = {
+//             postTitle: allPosts.title,
+//             postCategory: allPosts.category
+//             allPosts.location
+//             allPosts.body
 
 
 
-        }
+//         }
         
-    })
-    res.render("dashboard");
-});
+//     })
+//     res.render("dashboard");
+// });
 
 module.exports = router;
